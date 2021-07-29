f19cc1d0e178bbcb410798022b16020d1601af77
Ashley Medway <ashley.medway@proteansoftware.co.uk>
2021-07-29 11:41:42 +0100

    Fix not initaliseing iOS request properly

--- ios/Plugin/CapacitorSquare.swift
+++ ios/Plugin/CapacitorSquare.swift
@@ -1,10 +1,12 @@
 import Foundation
+import SquarePointOfSaleSDK
 
 @objc public class CapacitorSquare: NSObject {
     static var ApplicationId = "";
 
     @objc public func setApplicationID(_ applicationId: String) {
         CapacitorSquare.ApplicationId = applicationId;
+        SCCAPIRequest.setApplicationID(applicationId);
     }
 
     @objc public func isInitalised() -> Bool {
