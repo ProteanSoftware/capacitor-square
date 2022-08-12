77f6a39f805e553a96666880d5aca7cd27b07aa3
Ashley Medway <ashley.medway@proteansoftware.co.uk>
2022-08-12 20:51:57 +0100

    Support for AutoReturn (android)

--- src/definitions.ts
+++ src/definitions.ts
@@ -7,14 +7,48 @@ export type TransactionCompletedListener = (callback: {
 
 export type TransactionFailedListener = (callback: { error: any }) => void;
 
+/**
+ * AutoReturn timeout values
+ */
+export enum AutoReturn {
+  /**
+   * No timeout
+   */
+  NoTimeout = 0,
+  /**
+   * Minimum timeout value (3200 milliseconds)
+   */
+  Min = 3200,
+  /**
+   * Maximum timeout value (10000 milliseconds)
+   */
+  Max = 10000
+};
+
 export interface CapacitorSquarePlugin {
   initApp(options: { applicationId: string }): Promise<{ message: string }>;
 
   startTransaction(options: {
+    /**
+     * amount in pennies/cents
+     */
     totalAmount: number,
+    /**
+     * // ISO currency code, must be support by square
+     */
     currencyCode: string,
-    allowedPaymentMethods?: string[],
-    callbackUrl?: string
+    /**
+     * Sqaure TendType: https://developer.squareup.com/docs/api/point-of-sale/android/com/squareup/sdk/pos/ChargeRequest.TenderType.html
+     */
+    allowedPaymentMethods?: string[] | null,
+    /**
+     * The timeout to set in milliseconds, or AutoReturn.NoTimeout. If you specify a timeout, it must be between 3200 milliseconds and 10000 milliseconds.
+     */
+    autoReturnTimeout?: number | AutoReturn.NoTimeout | null,
+    /**
+     * see iOS setup
+     */
+    callbackUrl?: string | null
   }): Promise<void>;
 
   handleIosResponse(options: { url: string }): Promise<void>;
